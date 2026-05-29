efdb763d1ad0a7f6d69e936b26f72fd24aaeb052
John Warsop <john.warsop@qkey.org>
2026-05-29 22:52:58 +0000

    File changes

--- src/hooks/useQueryString.js
+++ src/hooks/useQueryString.js
@@ -1,13 +1,17 @@
+import { useLocation } from "react-router-dom";
+
 /**
  * Returns the current URL search parameters as a query string (including the leading '?')
  * Use this to preserve URL parameters when navigating between pages.
+ * This hook updates when the URL changes.
  * 
  * Example:
  * const queryString = useQueryString();
  * <Link to={`/contacts${queryString}`}>Contacts</Link>
  */
 export function useQueryString() {
-  const params = new URLSearchParams(typeof window !== 'undefined' ? window.location.search : '');
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
   const search = params.toString();
   return search ? `?${search}` : '';
 }
